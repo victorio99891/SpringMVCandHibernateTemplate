78993ed6499dcebd58a0a6e920acfd127aabb3a8
Wiktor Krzyżanowski <victorio99891@gmail.com>
2018-09-07 13:15:01 +0200

    Working template.

---
+++ out/artifacts/CustomerTracker_war_exploded/WEB-INF/views/list-customers.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+List customers coming soon...
+</body>
+</html>
---
+++ src/com/wiktor/Controller/CustomerController.java
@@ -0,0 +1,21 @@
+package com.wiktor.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @RequestMapping("/list")
+    public String listCustomers(Model model) {
+
+
+
+
+        return "list-customers";
+    }
+
+}
